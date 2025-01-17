bb3f2af6d9ca414afa6b556688cb4fe2f4513f5a
Ryan Chu <rchu39@my.bcit.ca>
2025-01-16 22:41:50 -0800

    Almost completed BankAccount.java, added compareTo() to Date.java.

--- src/code/ca/bcit/comp2522/bank/BankAccount.java
+++ src/code/ca/bcit/comp2522/bank/BankAccount.java
@@ -1,5 +1,214 @@
 package ca.bcit.comp2522.bank;
 
-public class BankAccount
+
+/**
+ * Represents a bank account.
+ * Keeps track of the owner, their pin, the account number, and the date opened/closed.
+ * Has the functionality to deposit or withdraw (w or w/o pin verification) money in usd.
+ *
+ * @Author Ryan Chu
+ * @Version 2024
+ */
+class BankAccount
 {
+    private static int NOTHING                   = 0;
+    private static int ACCOUNT_NUMBER_MIN_LENGTH = 6;
+    private static int ACCOUNT_NUMBER_MAX_LENGTH = 7;
+
+
+    private final BankClient client;
+    private final int        pin;
+    private final String     accountNumber;
+    private final Date       accountOpened;
+    private final Date       accountClosed;
+
+    private double balanceUsd;
+
+    /**
+     * Constructs a BankAccount.
+     *
+     * @param client        the bank accounts owner
+     * @param pin           the PIN of the account
+     * @param accountNumber the unique account number
+     * @param accountOpened when the account was opened
+     * @param accountClosed when the account was closed
+     * @param balanceUsd    the current balance of the account in USD
+     */
+    public BankAccount(final BankClient client,
+                       final int pin,
+                       final String accountNumber,
+                       final Date accountOpened,
+                       final Date accountClosed,
+                       final double balanceUsd)
+    {
+        validateClient(client);
+        validatePin(pin);
+        validateAccountNumber(accountNumber);
+        validateDate(accountOpened);
+        validateDate(accountClosed);
+        validateClientDateConsistency(client,
+                                      accountOpened,
+                                      accountClosed);
+        validateBalanceUsd(balanceUsd);
+
+        this.client = new BankClient();
+        this.pin    = 0;
+        //String length 6 or 7
+        this.accountNumber = "";
+        this.accountOpened = null;
+        this.accountClosed = null;
+        this.balanceUsd    = 0;
+    }
+
+    private void validateClient(final BankClient client)
+    {
+        if(client == null)
+        {
+            throw new NullPointerException("Bank client is null");
+        }
+    }
+
+    private void validatePin(final int pin)
+    {
+        if(pin < 0)
+        {
+            throw new IllegalArgumentException("Pin number out of range");
+        }
+    }
+
+    private void validateAccountNumber(final String accountNumber)
+    {
+        if(accountNumber == null || accountNumber.length() < ACCOUNT_NUMBER_MIN_LENGTH ||
+           accountNumber.length() > ACCOUNT_NUMBER_MAX_LENGTH)
+        {
+            throw new IllegalArgumentException(
+                    "Invalid account number length, must be between " + ACCOUNT_NUMBER_MIN_LENGTH + " and " +
+                    ACCOUNT_NUMBER_MAX_LENGTH + " got: " + accountNumber.length());
+        }
+    }
+
+    private void validateDate(final Date date)
+    {
+        if(date == null)
+        {
+            throw new NullPointerException("Given date is null");
+        }
+    }
+
+    private void validateBalanceUsd(final double number)
+    {
+        if(number <= NOTHING)
+        {
+            throw new IllegalArgumentException("Balance given must be positive, got: " + number);
+        }
+    }
+
+    private void validateClientDateConsistency(final BankClient client,
+                                               final Date accountOpened,
+                                               final Date accountClosed)
+    {
+
+    }
+
+    /**
+     * Retrieves the client associated with the bank account.
+     *
+     * @return the BankClient object representing the client associated with the account.
+     */
+    public BankClient getClient()
+    {
+        return client;
+    }
+
+    /**
+     * Retrieves the PIN code associated with the account.
+     *
+     * @return the PIN code for the account.
+     */
+    public int getPin()
+    {
+        return pin;
+    }
+
+    /**
+     * Retrieves the account number.
+     *
+     * @return the account number as a String.
+     */
+    public String getAccountNumber()
+    {
+        return accountNumber;
+    }
+
+    /**
+     * Retrieves the date when the account was opened.
+     *
+     * @return the Date when the account was opened.
+     */
+    public Date getAccountOpened()
+    {
+        return accountOpened;
+    }
+
+    /**
+     * Retrieves the date when the account was closed.
+     * If the account is still open, this value may be null.
+     *
+     * @return the Date when the account was closed, or null if the account is still open.
+     */
+    public Date getAccountClosed()
+    {
+        return accountClosed;
+    }
+
+    /**
+     * Retrieves the balance of the account in USD.
+     *
+     * @return the current balance in USD.
+     */
+    public double getBalanceUsd()
+    {
+        return balanceUsd;
+    }
+
+
+    /**
+     * @param amountUsd
+     */
+    public void deposit(final double amountUsd)
+    {
+        validateBalanceUsd(amountUsd);
+        this.balanceUsd += amountUsd;
+    }
+
+
+    public double withdraw(final double amountUsd)
+    {
+        validateBalanceUsd(amountUsd);
+        this.balanceUsd -= amountUsd;
+        return amountUsd;
+    }
+
+
+    public double withdraw(final double amountUsd,
+                           final int pinToMatch)
+    {
+        validateBalanceUsd(amountUsd);
+
+        if(pinToMatch == this.pin)
+        {
+            this.balanceUsd -= amountUsd;
+        }
+        else
+        {
+            System.out.println("Invalid pin");
+        }
+        return amountUsd;
+    }
+
+
+    public void getDetails()
+    {
+
+    }
 }
--- src/code/ca/bcit/comp2522/bank/Date.java
+++ src/code/ca/bcit/comp2522/bank/Date.java
@@ -1,51 +1,56 @@
 package ca.bcit.comp2522.bank;
 
+
 /**
  * @author Ryan Chu
  * @version 1.0
  */
 class Date
 {
-    private final static int MIN_YEAR = 1800;
-    private final static int MAX_YEAR = 2024;
+    //first of month/last of month
+    private final static int MIN_YEAR  = 1800;
+    private final static int MAX_YEAR  = 2024;
     private final static int MIN_MONTH = 1;
     private final static int MAX_MONTH = 12;
-    private final static int MIN_DAY = 1;
+    private final static int MIN_DAY   = 1;
 
-    private final static int MAX_DAY_LONG_MONTH = 31;
+    //short month long month max
+    private final static int MAX_DAY_LONG_MONTH  = 31;
     private final static int MAX_DAY_SHORT_MONTH = 30;
-    private final static int MAX_LEAP_FEB_DAY = 29;
+    private final static int MAX_LEAP_FEB_DAY    = 29;
     private final static int MAX_NO_LEAP_FEB_DAY = 28;
 
-    private final static int JANUARY = 1;
-    private final static int FEBRUARY = 2;
-    private final static int MARCH = 3;
-    private final static int APRIL = 4;
-    private final static int MAY = 5;
-    private final static int JUNE = 6;
-    private final static int JULY = 7;
-    private final static int AUGUST = 8;
+    private final static int JANUARY   = 1;
+    private final static int FEBRUARY  = 2;
+    private final static int MARCH     = 3;
+    private final static int APRIL     = 4;
+    private final static int MAY       = 5;
+    private final static int JUNE      = 6;
+    private final static int JULY      = 7;
+    private final static int AUGUST    = 8;
     private final static int SEPTEMBER = 9;
-    private final static int OCTOBER = 10;
-    private final static int NOVEMBER = 11;
-    private final static int DECEMBER = 12;
+    private final static int OCTOBER   = 10;
+    private final static int NOVEMBER  = 11;
+    private final static int DECEMBER  = 12;
 
-
-    private final static int LEAP_YEAR_CYCLE = 4;
+    //year per leap/per century/years between leap centuroes
+    private final static int LEAP_YEAR_CYCLE           = 4;
     private final static int LEAP_YEAR_CYCLE_EXCEPTION = 100;
-    private final static int LEAP_YEAR_VALID_CHECK = 0;
+    private final static int LEAP_YEAR_VALID_CHECK     = 0;
 
 
-    private final static int NINETEENTH_CENTURY = 1800;
-    private final static int TWENTIETH_CENTURY = 1900;
+    private final static int NINETEENTH_CENTURY   = 1800;
+    private final static int TWENTIETH_CENTURY    = 1900;
     private final static int TWENTY_FIRST_CENTURY = 2000;
-    private final static int SATURDAY = 0;
-    private final static int SUNDAY = 1;
-    private final static int MONDAY = 2;
-    private final static int TUESDAY = 3;
-    private final static int WEDNESDAY = 4;
-    private final static int THURSDAY = 5;
-    private final static int FRIDAY = 6;
+    private final static int SATURDAY             = 0;
+    private final static int SUNDAY               = 1;
+    private final static int MONDAY               = 2;
+    private final static int TUESDAY              = 3;
+    private final static int WEDNESDAY            = 4;
+    private final static int THURSDAY             = 5;
+    private final static int FRIDAY               = 6;
+
+    private final static int NO_DIFFERENCE       = 0;
 
 
     private final int year;
@@ -53,17 +58,23 @@ class Date
     private final int day;
 
 
-    Date(final int year,
-         final int month,
-         final int day)
+    public Date(final int year,
+                final int month,
+                final int day)
     {
-        validateInt(year, MIN_YEAR, MAX_YEAR);
-        validateInt(month, MIN_MONTH, MAX_MONTH);
-        validateDay(day, month, year);
+        validateInt(year,
+                    MIN_YEAR,
+                    MAX_YEAR);
+        validateInt(month,
+                    MIN_MONTH,
+                    MAX_MONTH);
+        validateDay(day,
+                    month,
+                    year);
 
-        this.year = year;
+        this.year  = year;
         this.month = month;
-        this.day = day;
+        this.day   = day;
     }
 
     private static void validateInt(final int number,
@@ -100,24 +111,33 @@ class Date
             case AUGUST:
             case OCTOBER:
             case DECEMBER:
-                validateInt(day, MIN_DAY, MAX_DAY_LONG_MONTH);
+                validateInt(day,
+                            MIN_DAY,
+                            MAX_DAY_LONG_MONTH);
                 break;
 
             case APRIL:
             case JUNE:
             case SEPTEMBER:
             case NOVEMBER:
-                validateInt(day, MIN_DAY, MAX_DAY_SHORT_MONTH);
+                validateInt(day,
+                            MIN_DAY,
+                            MAX_DAY_SHORT_MONTH);
                 break;
 
             case FEBRUARY:
-                if(isLeapYear(month, year))
+                if(isLeapYear(month,
+                              year))
                 {
-                    validateInt(day, MIN_DAY, MAX_LEAP_FEB_DAY);
+                    validateInt(day,
+                                MIN_DAY,
+                                MAX_LEAP_FEB_DAY);
                 }
                 else
                 {
-                    validateInt(day, MIN_DAY, MAX_NO_LEAP_FEB_DAY);
+                    validateInt(day,
+                                MIN_DAY,
+                                MAX_NO_LEAP_FEB_DAY);
                 }
 
             default:
@@ -139,7 +159,9 @@ class Date
     private static boolean isLeapYear(final int month,
                                       final int year)
     {
-        return (year % LEAP_YEAR_CYCLE == LEAP_YEAR_VALID_CHECK && year % LEAP_YEAR_CYCLE_EXCEPTION != LEAP_YEAR_VALID_CHECK) || (year % (LEAP_YEAR_CYCLE * LEAP_YEAR_CYCLE_EXCEPTION) == LEAP_YEAR_VALID_CHECK);
+        return (year % LEAP_YEAR_CYCLE == LEAP_YEAR_VALID_CHECK &&
+                year % LEAP_YEAR_CYCLE_EXCEPTION != LEAP_YEAR_VALID_CHECK) ||
+               (year % (LEAP_YEAR_CYCLE * LEAP_YEAR_CYCLE_EXCEPTION) == LEAP_YEAR_VALID_CHECK);
     }
 
     /**
@@ -191,8 +213,8 @@ class Date
     /**
      * Returns the day of the week as a String
      * <p>
-     * To get the day of the week, do the following seven steps for dates in the 1900s:
-     * e.g. October 31 1977:
+     * To get the day of the week, does the following seven steps for dates in the 1900s:
+     * e.g. October 31, 1977:
      * step 1: calculate the number of twelves in 77: 6
      * step 2: calculate the remainder from step 1: 77 - 12*6 = 77 - 72 = 5
      * step 3: calculate the number of fours in step 2: 5/4 = 1.25, so 1
@@ -210,54 +232,62 @@ class Date
      */
     public String getDatOfTheWeek()
     {
-        final int initalFormulaValue;
+        int initialFormulaValue;
 
-        final int step1result;
-        final int step2result;
-        final int step3result;
-        final int step4result;
-        final int step5result;
-        final int step6result;
+        final int    step1result;
+        final int    step2result;
+        final int    step3result;
+        final int    step4result;
+        final int    step5result;
+        final int    step6result;
         final String step7result;
 
-        final int step1Constant;
-        final int step3Constant;
+        final int    step1Constant;
+        final int    step3Constant;
         final String step5Constant;
-        final int step5Offset;
-        final int step6Constant;
-        final int nineteenthCenturyOffset;
-        final int twentiethCenturyOffset;
-        final int twentyFirstCenturyOffset;
+        final int    step5Offset;
+        final int    step6Constant;
+        final int    nineteenthCenturyOffset;
+        final int    twentiethCenturyOffset;
+        final int    twentyFirstCenturyOffset;
+        final int    leapYearOffset;
 
-        step1Constant = 12;
-        step3Constant = 4;
-        step5Constant = "144025036146";
-        step5Offset = 1;
-        step6Constant = 7;
-        nineteenthCenturyOffset = 2;
-        twentiethCenturyOffset = 0;
+        step1Constant            = 12;
+        step3Constant            = 4;
+        step5Constant            = "144025036146";
+        step5Offset              = 1;
+        step6Constant            = 7;
+        nineteenthCenturyOffset  = 2;
+        twentiethCenturyOffset   = 0;
         twentyFirstCenturyOffset = 6;
+        leapYearOffset           = 6;
 
         if(year >= NINETEENTH_CENTURY && year < TWENTIETH_CENTURY)
         {
-            initalFormulaValue = nineteenthCenturyOffset + this.year - NINETEENTH_CENTURY;
+            initialFormulaValue = nineteenthCenturyOffset + this.year - NINETEENTH_CENTURY;
         }
         else if(year >= TWENTIETH_CENTURY && year < TWENTY_FIRST_CENTURY)
         {
-            initalFormulaValue = twentiethCenturyOffset + this.year - TWENTIETH_CENTURY;
+            initialFormulaValue = twentiethCenturyOffset + this.year - TWENTIETH_CENTURY;
         }
         else if(year >= TWENTY_FIRST_CENTURY && year < MAX_YEAR)
         {
-            initalFormulaValue = twentyFirstCenturyOffset + this.year - TWENTY_FIRST_CENTURY;
+            initialFormulaValue = twentyFirstCenturyOffset + this.year - TWENTY_FIRST_CENTURY;
         }
         else
         {
             throw new IllegalStateException("Invalid year: " + year);
         }
 
+        if((this.month == JANUARY || this.month == FEBRUARY) && isLeapYear(this.month,
+                                                                           this.year))
+        {
+            initialFormulaValue += leapYearOffset;
+        }
 
-        step1result = initalFormulaValue / step1Constant;
-        step2result = initalFormulaValue - step1result;
+
+        step1result = initialFormulaValue / step1Constant;
+        step2result = initialFormulaValue - step1result;
         step3result = step2result % step3Constant;
         step4result = this.day + step1result + step2result + step3result;
         step5result = step4result + Character.getNumericValue(step5Constant.charAt(this.month - step5Offset));
@@ -297,5 +327,34 @@ class Date
         return step7result;
     }
 
+    /**
+     * Compares this date with the specified date to determine their relative order.
+     *
+     * <p>
+     * returns the difference of the calling date's year with the given date's year
+     * or the difference in months if they are the same year
+     * or the difference in days if they are the same month
+     * </p>
+     *
+     * @param dateToCompare the Date object to compare this date against
+     * @return a negative int, positive int, or zero
+     */
+    public int compareTo(Date dateToCompare)
+    {
+        int comparison;
+
+        comparison = this.year - dateToCompare.getYear();
+
+        if(comparison == NO_DIFFERENCE)
+        {
+            comparison = this.month - dateToCompare.getMonth();
+            if(comparison == NO_DIFFERENCE)
+            {
+                comparison = this.day - dateToCompare.getDay();
+            }
+        }
+
+        return comparison;
+    }
 }
 
